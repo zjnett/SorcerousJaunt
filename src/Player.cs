aa96e35160cb327ef33e8196572442cd6296b352
Zach Nett <zjnett@mac.com>
2023-10-19 19:59:31 -0700

    Add initial player movement

--- src/Player.cs
+++ src/Player.cs
@@ -3,4 +3,34 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
+    [Signal]
+    public delegate void GameOverEventHandler();
+
+    [Export]
+    private const float Gravity = 2000f;
+    [Export]
+    private const float FloatSpeed = 4000f;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        var velocity = Velocity;
+        velocity.Y += Gravity * (float)delta;
+
+        if (Input.IsActionPressed("jump"))
+        {
+            // Zero out velocity
+            Velocity = new Vector2(0, 0);
+            velocity.Y -= FloatSpeed * (float)delta;
+        }
+
+        Velocity = velocity;
+
+        var motion = velocity * (float)delta;
+        MoveAndCollide(motion);
+    }
+
+    public void Die()
+    {
+        // Handle player death here
+    }
 }
